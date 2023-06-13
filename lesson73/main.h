10f4902aeb77ce5a5d7cf6556d3756467e0147bd
DtlipKbf <lizabeta.li.p@gmail.com>
2023-06-13 18:43:10 +0300

    Add project files.

---
+++ lesson73/main.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream> //cout, cin, endl
+#include <string>
+#include <fstream> //ofstream class, ifstream class
+//#include <stdlib.h> // printf, sccanf
+
+using namespace std;
